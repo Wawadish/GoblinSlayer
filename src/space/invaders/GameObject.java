969f26ae76fa21de3fb921f5d845fb32a8ebbeeb
TheBerg974 <32581690+TheBerg974@users.noreply.github.com>
2018-11-19 16:25:38 -0500

    backend #1

---
+++ src/space/invaders/GameObject.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package space.invaders;
+
+import javafx.scene.shape.Rectangle;
+
+/**
+ *
+ * @author Sam
+ */
+public abstract class GameObject extends Rectangle {
+    
+    private double x;
+    private double y;
+    private double width;
+    private double height;
+
+    public GameObject(double x, double y, double width, double height) {
+        super(x, y, width, height);
+    }
+    
+    abstract boolean collides(GameObject o);
+    
+    abstract void update();
+    
+    abstract void animate();
+    
+}
---
+++ src/space/invaders/Projectile.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package space.invaders;
+
+/**
+ *
+ * @author Sam
+ */
+public class Projectile extends GameObject {
+    
+     private Vector2D position;
+    private Vector2D velocity;
+    private Vector2D acceleration;
+
+    public Projectile(Vector2D position, Vector2D velocity, Vector2D acceleration, double x, double y, double width, double height) {
+        super(x, y, width, height);
+        this.position = position;
+        this.velocity = velocity;
+        this.acceleration = acceleration;
+    }
+
+    @Override
+    boolean collides(GameObject gameObject1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    void update() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    void animate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
