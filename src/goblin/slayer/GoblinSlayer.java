f69adc90235f70f6e50f34e083e3a1b655e46ab1
Wawadish <wawadish@gmail.com>
2018-12-04 00:36:28 -0500

    Removed useless files

--- src/goblin/slayer/GoblinSlayer.java
+++ src/goblin/slayer/GoblinSlayer.java
@@ -8,42 +8,45 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class GoblinSlayer extends Application {
-    
+
     private static Stage window;
     private static Scene scene;
     public int difficulty;
-    
-    
+
     @Override
     public void start(Stage primaryStage) {
         window = primaryStage;
         window.setResizable(false);
-        
+
         AssetManager.preloadAllAssets();
         MenuPane menu = new MenuPane(1);
-        
+
         scene = new Scene(menu, 1280, 720);
-        
+
         //the value of paused is not being printed.
-        
         window.setTitle("Goblin Slayer");
-        
+
         window.setScene(scene);
         window.sizeToScene();
-        
-        window.setOnCloseRequest(e ->{
+
+        window.setOnCloseRequest(e -> {
             Platform.exit();
             System.exit(0);
         });
-        
-        window.show(); 
+
+        window.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        //Just in case
+        try {
+            launch(args);
+        } catch (Exception e) {
+
+        }
     }
-    
-    public static void changePane(Pane p){
+
+    public static void changePane(Pane p) {
         scene = new Scene(p, 1280, 720);
         window.setScene(scene);
         p.requestFocus();
